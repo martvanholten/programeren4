0c8d4106eb4883d3afa5c878b41fd653956c9120
Mart <martvanholten@hotmail.com>
2022-07-04 20:33:09 +0200

    dbtest

--- test/integration/meal.test.js
+++ test/integration/meal.test.js
@@ -1,4 +1,4 @@
-process.env.DB_DATABASE = process.env.DB_DATABASE || 'share-a-meal'
+process.env.DB_DATABASE = process.env.DB_DATABASE || 'test'
 process.env.LOGLEVEL = 'warn'
 
 const chai = require('chai')
